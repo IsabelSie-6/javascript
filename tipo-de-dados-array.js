050217f4e0178647e1f1c111d23fbf8c93640c85
IsabelSie-6 <isa.siebert.10@gmail.com>
2026-09-11 10:31:23 -0300

    add tipo de dados arrays atualizado 1109

--- tipo-de-dados-array.js
+++ tipo-de-dados-array.js
@@ -1,3 +1,7 @@
 let frutas = ["maçã", "banana", "laranja"]; // array de strings
 let numeros = [1, 2, 3, 4, 5]; // Array de números 
 let misto = [true, "texto", 42]; // Array com diferentes tipos de dados
+
+console.log(frutas)
+console.log(numeros)
+console.log(misto)

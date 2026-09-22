75f81385aa96af028d5f91df83c7ba96e6817fea
IsabelSie-6 <isa.siebert.10@gmail.com>
2026-09-22 08:35:11 -0300

    add if js 2209

---
+++ if.js
@@ -0,0 +1,5 @@
+let hora = 15; 
+if (hora < 12) { 
+    console.log("Bom dia");
+} // O restante do código continua independentemente se a condição for verdadeira ou falsa. 
+console.log( "Dinheiro não traz felicidade. Me dê o seu e seja feliz. ") 
